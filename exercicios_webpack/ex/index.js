bb36e891bb68eae32b0408fb6a4e86c485c36a68
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-09-06 10:39:11 -0300

    Aula 2 - Webpack - 14 - Interpretando o Operador Spread (Plugin Babel)

--- exercicios_webpack/ex/index.js
+++ exercicios_webpack/ex/index.js
@@ -1,5 +1,19 @@
 import ClassePessoa from './ClassePessoa';
-
 const pessoa = new ClassePessoa('Ollyver Ottoboni, agora Pietra - Alterado de novo - Outra vez - 2');
-
 console.log(pessoa.toString());
+
+const produto = {
+    nome: 'Caneta BIC Preta',
+    preco: 1.90,
+    desconto: 0.05
+}
+
+function clone(objeto) {
+
+    return {...objeto}
+}
+
+const novoProduto = clone(produto);
+novoProduto.nome = 'Caneta AZUL BIC';
+
+console.log('Produto: ', produto, 'Novo Produto: ', novoProduto);
