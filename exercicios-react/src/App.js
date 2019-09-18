092dec6c58de98b4df0e51bce580daef918f54ca
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-09-18 11:37:29 -0300

    Aula 3 - React - 23 - Usando as Propriedades do Componente - Passando as props da index.js para a App.js

--- exercicios-react/src/App.js
+++ exercicios-react/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const App = ()=> (
+const App = (props)=> (
     <div>
-        <h1>Exercícios React</h1>
+        <h1>Exercícios React {props.value}</h1>
     </div>
 );
 
