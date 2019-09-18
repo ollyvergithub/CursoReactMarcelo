8148ea6448ba87454e0ced8dccd0c555215cc2d8
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-09-18 13:17:21 -0300

    Aula 3 - React - 26 - Componentes Filhos - Criando Componente Family e utilizando o props.children

--- exercicios-react/src/App.js
+++ exercicios-react/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import SilvaFamily from './Componentes/silvaFamily';
+import SilvaFamily from './Componentes/SilvaFamily';
+import Family from './Componentes/Family';
+import Member from './Componentes/Member';
 
 const App = ()=> (
     <div className="container">
         <h1>Exercícios React </h1>
-        <SilvaFamily/>
+        {/*<SilvaFamily/>*/}
+        <Family>
+            <Member name="Ollyver" lastName="Ottoboni"/>
+            <Member name="Pietra" lastName="Eu Te Amo"/>
+        </Family>
+
     </div>
 );
 
---
+++ exercicios-react/src/Componentes/Family.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => (
+    <div>
+        <h1>Componente Family, usando props.children</h1>
+        {props.children}
+    </div>
+
+);
